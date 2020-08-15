110aa9fce820da44ec0771e4fddbc255f0d529ba
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-15 05:14:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,8 +28,8 @@ def long_planeteer_calls(array)# code an argument here
   end
 end
 
-puts long_planeteer_calls(["hi", "hel", "hello"]
-puts long_planeteer_calls(["hi", "hel", "hola"]
+puts long_planeteer_calls(["hi", "hel", "hello"])
+puts long_planeteer_calls(["hi", "hel", "hola"])
 
 
 def find_the_cheese# code an argument here
