edd69db6fb4a4c3ce7be752c94dbad25d57c9d7a
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-15 05:04:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,12 @@ def roll_call_dwarves(array)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  newarray = array.map do |item|
+    "#{item.capitalize}!"
+  end 
+  
 end
 
 def long_planeteer_calls# code an argument here
