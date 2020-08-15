9cc7ced09d9f5ed6a34c4f0d0ea46f6aebe33dc1
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-15 05:13:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,14 @@ def summon_captain_planet(array)# code an argument here
 end
 puts  summon_captain_planet(["hi", "hello"])
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  result = array.find do |item|
+    item.size > 4
+  end
+  if result
+    puts result
+    return true
 end
 
 def find_the_cheese# code an argument here
