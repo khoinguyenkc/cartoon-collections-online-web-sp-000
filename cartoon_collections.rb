f4ccda97b1d0b8e2b1efb5510039ce504df4651d
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-15 05:19:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ end
 
 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |item|
