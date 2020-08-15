3ced44c3b186c762fc4fa15127b697dd2cb3f0f4
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-15 05:04:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)# code an argument here
   newarray = array.map do |item|
     "#{item.capitalize}!"
   end 
-  
+
 end
 
 def long_planeteer_calls# code an argument here
